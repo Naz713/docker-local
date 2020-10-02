6ee7749d9196d97c94bce2950e46229639b6912e
Nazareth Rangel <naz7771313@gmail.com>
2020-10-02 13:56:32 -0500

    RegisterController: check for complete parameters

--- carriers/src/Controller/RegisterController.php
+++ carriers/src/Controller/RegisterController.php
@@ -12,6 +12,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RegisterController extends AbstractController
 {
@@ -23,6 +26,58 @@ class RegisterController extends AbstractController
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
         ]);
+    }
+
+    /**
+     * @Route("/saveRegister", name="save_register_action")
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function registerAction(Request $request)
+    {
+        $carrierId = $request->get('carrier_id');
+        $carrierName = $request->get('carrier_name');
+        $carrierEmail = $request->get('carrier_email');
+        $scac = $request->get('scac');
+        $providerId = $request->get('provider_id');
+        $providerName = $request->get('provider_name');
+        $providerEmail = $request->get('provider_email');
+        $notes = $request->get('notes');
+
+        $this->get("logger")->debug("
+            carrierId : $carrierId
+            carrierName : $carrierName
+            carrierEmail : $carrierEmail
+            scac : $scac
+            providerId : $providerId
+            providerName : $providerName
+            providerEmail : $providerEmail
+            notes : $notes ");
+
+        if ( strlen($carrierId)<0 && (strlen($carrierName)<0 || strlen($carrierEmail)<0) )
+        {
+            return new JsonResponse(array(
+                "code" => 400,
+                "msg" => "Carrier Required"
+            ));
+        }
+
+        if ( strlen($providerId)<0 && (strlen($providerName)<0 || strlen($providerEmail)<0) )
+        {
+            return new JsonResponse(array(
+                "code" => 400,
+                "msg" => "Provider Required"
+            ));
+        }
+
+        //TODO insert on db
+
+        //TODO Send email
+
+        return new JsonResponse(array(
+            "code" => 200,
+        ));
     }
 }
 
